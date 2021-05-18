396928ccb505b89eb0179f8ebb1f9493d4111df1
Dan Fairburn <danyal.fairburn@bt.com>
2021-05-18 10:25:06 +0100

    Get a sphere rendering in a panel

--- src/HydraWorld.tsx
+++ src/HydraWorld.tsx
@@ -7,42 +7,61 @@ interface Props extends PanelProps<SimpleOptions> {}
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  const renderer = new THREE.WebGLRenderer();
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  let root;
+  const renderer = new THREE.WebGLRenderer( { alpha: true });
+  const light = new THREE.PointLight( 0xff0000, 1, 100 );
+  const sphereGeometry = new THREE.SphereGeometry( 2, 32, 32 );
+  const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+  const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+  let camera: THREE.PerspectiveCamera;
+
+  sphere.castShadow = true; //default is false
+  sphere.receiveShadow = false; //default
+
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+  light.position.set( 5, 5, 5);
+  light.castShadow = true
+
+  renderer.setPixelRatio( window.devicePixelRatio );
+
+  scene.add( light );
+  scene.add( sphere );
+
 
   useEffect(() => {
-    console.log("foo");
-    root = document.getElementById("hydra-world-root")
-    console.log(root)
+    const root = document.getElementById("hydra-world-root");
+    if (root === null) return
 
-    scene.add( cube );
-    camera.position.z = 5;
-    renderer.autoClear = false;
-    if (root) {
+    if (!root.hasChildNodes()) {
+      const {width, height} = root.getBoundingClientRect()
+      renderer.setSize(width, height);
+      camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+      camera.position.z = 5;
       root.appendChild(renderer.domElement);
+    } else {
+      const {width, height} = root.getBoundingClientRect()
+      camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+      camera.position.z = 5;
+      renderer.setSize(width, height);
+      root.replaceChild(renderer.domElement, root.childNodes[0]);
     }
-  });
-
-  useEffect(() => {
     animate();
-  }, [root, scene, camera])
+  });
 
   const animate = () => {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render(scene, camera)
   }
 
 
   return (
-      <div id={"hydra-world-root"}/>
+      <div 
+      id={"hydra-world-root"}
+      style={{
+        height: "inherit",
+        width: "inherit"
+      }}
+      />
   );
 };
